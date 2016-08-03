e625ea0d444335a49e16764a9c68bb2f35f7b0f2
YouXianMing <YouXianMing1987@126.com>
2016-08-03 16:21:26 +0800

    Fix sth not important.

--- Animations/UITextFieldView.m
+++ Animations/UITextFieldView.m
@@ -21,9 +21,10 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     
     if (self = [super initWithFrame:frame]) {
-    
-        self.textField          = [[UITextField alloc] initWithFrame:self.bounds];
-        self.textField.delegate = self;
+        
+        self.textField              = [[UITextField alloc] initWithFrame:self.bounds];
+        self.textField.delegate     = self;
+        self.textFieldViewValidator = [AbsTextFieldViewValidator new];
         [self addSubview:self.textField];
     }
     
@@ -31,12 +32,12 @@
 }
 
 - (void)becomeFirstResponder {
-
+    
     [self.textField becomeFirstResponder];
 }
 
 - (void)resignFirstResponder {
-
+    
     [self.textField resignFirstResponder];
 }
 
@@ -44,7 +45,7 @@
                               didBeginEditing:(textFieldDidBeginEditing_t)didBeginEditingBlock
                                 didEndEditing:(textFieldDidEndEditing_t)didEndEditingBlock
                                  shouldReturn:(textFieldShouldReturn_t)shouldReturnBlock {
-
+    
     self.shouldChangeCharactersInRangeBlock = block;
     self.shouldReturnBlock                  = shouldReturnBlock;
     self.didBeginEditingBlock               = didBeginEditingBlock;
@@ -52,44 +53,44 @@
 }
 
 - (void)setCurrentTextFieldText:(NSString *)text {
-
+    
     _currentText    = text;
     _textField.text = text;
 }
 
 - (void)createInputAccessoryViewWithViewHeight:(CGFloat)height block:(void (^)(UIView *inputAccessoryView, UITextFieldView *textFieldView))block {
-
+    
     CGRect rect                       = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, height);
     self.textField.inputAccessoryView = [[UIView alloc] initWithFrame:rect];
     block ? block(self.textField.inputAccessoryView, self) : 0;
 }
 
 - (TextFieldValidatorMessage *)checkingTheTextFieldViewString {
-
+    
     return [self.textFieldViewValidator validatorWithInputSting:self.currentText];
 }
 
 - (CGRect)rectFromView:(UIView *)view {
-
+    
     return [self convertRect:self.bounds toView:view];
 }
 
 #pragma mark - UITextFieldDelegate
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
-
+    
     if (self.shouldBeginEditingBlock) {
         
         return self.shouldBeginEditingBlock(self);
         
     } else {
-    
+        
         return YES;
     }
 }
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
-
+    
     if (self.didBeginEditingBlock) {
         
         self.didBeginEditingBlock(self);
@@ -102,19 +103,19 @@
 }
 
 - (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
-
+    
     if (self.shouldEndEditingBlock) {
         
         return self.shouldEndEditingBlock(self);
         
     } else {
-    
+        
         return YES;
     }
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-
+    
     if (self.didEndEditingBlock) {
         
         self.didEndEditingBlock(self);
@@ -122,7 +123,7 @@
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
-
+    
     if (self.textField.secureTextEntry == YES && _secureTextEntryBecomeActive == YES) {
         
         // 密码键盘特殊处理
@@ -130,7 +131,7 @@
         self.secureTextEntryBecomeActive = NO;
         
     } else {
-    
+        
         // 普通键盘
         NSString *currentText = [textField.text stringByReplacingCharactersInRange:range withString:string];
         self.currentText      = currentText;
@@ -146,33 +147,44 @@
         return self.shouldChangeCharactersInRangeBlock(self, range, string, self.currentText);
         
     } else {
-    
+        
         return YES;
     }
 }
 
 - (BOOL)textFieldShouldClear:(UITextField *)textField {
-
+    
     if (self.shouldClearBlock) {
         
         return self.shouldClearBlock(self);
         
     } else {
-    
+        
         return YES;
     }
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-
+    
     if (self.shouldReturnBlock) {
         
         return self.shouldReturnBlock(self);
         
     } else {
-    
+        
         return YES;
     }
+}
+
+- (void)dealloc {
+    
+    self.shouldBeginEditingBlock            = nil;
+    self.didBeginEditingBlock               = nil;
+    self.shouldEndEditingBlock              = nil;
+    self.didEndEditingBlock                 = nil;
+    self.shouldChangeCharactersInRangeBlock = nil;
+    self.shouldClearBlock                   = nil;
+    self.shouldReturnBlock                  = nil;
 }
 
 @end
