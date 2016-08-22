0dfe39611e7ef17676f71b0b25943ced7bd38e5f
YouXianMing <YouXianMing1987@126.com>
2016-08-22 14:19:57 +0800

    Fix sth in PageFlipEffectController.m

--- Animations/PageFlipEffectController.m
+++ Animations/PageFlipEffectController.m
@@ -12,7 +12,6 @@
 #import "UIView+SetRect.h"
 #import "POP.h"
 
-
 @interface PageFlipEffectController ()
 
 @property (nonatomic, strong) CALayer *layer;
@@ -30,16 +29,16 @@
     CGSize   size  = [Math resetFromSize:image.size withFixedWidth:Width / 2.f];
     
     // layer
-    _layer               = [CALayer layer];
-    _layer.anchorPoint   = CGPointMake(1.f, 0.5f);
-    _layer.frame         = CGRectMake(0, 0, Width / 2.f, size.height);
-    _layer.x             = 0;
-    _layer.position      = CGPointMake(Width / 2.f, self.contentView.middleY);
-    _layer.contents      = (__bridge id)([UIImage imageNamed:@"pic_1"].CGImage);
-    _layer.borderColor   = [UIColor blackColor].CGColor;
-    _layer.borderWidth   = 4.f;
-    _layer.masksToBounds = YES;
-    _layer.transform     = CATransform3DMakeRotation([Math radianFromDegree:0], 0.0, 1.0, 0.0);
+    _layer                        = [CALayer layer];
+    _layer.anchorPoint            = CGPointMake(1.f, 0.5f);
+    _layer.frame                  = CGRectMake(0, 0, Width / 2.f, size.height);
+    _layer.position               = CGPointMake(Width / 2.f, self.contentView.middleY);
+    _layer.contents               = (__bridge id)([UIImage imageNamed:@"pic_1"].CGImage);
+    _layer.borderColor            = [UIColor blackColor].CGColor;
+    _layer.borderWidth            = 4.f;
+    _layer.masksToBounds          = YES;
+    _layer.allowsEdgeAntialiasing = YES; // 抗锯齿
+    _layer.transform              = CATransform3DMakeRotation([Math radianFromDegree:0], 0.0, 1.0, 0.0);
     [self.contentView.layer addSublayer:_layer];
     
     // 一元一次方程
@@ -66,48 +65,21 @@
     
     [CATransaction setDisableActions:YES];
     _layer.transform = perspectiveTransform;
-    
-    if (x >= Width / 2.f) {
-        
-        [CATransaction setDisableActions:YES];
-        _layer.contents = (__bridge id)([UIImage imageNamed:@"pic_2"].CGImage);
-        
-    } else {
-        
-        [CATransaction setDisableActions:YES];
-        _layer.contents = (__bridge id)([UIImage imageNamed:@"pic_1"].CGImage);
-    }
+    _layer.contents  = (__bridge id)([UIImage imageNamed:x >= Width / 2.0f ? @"pic_2" : @"pic_1"].CGImage);
     
     if (sender.state == UIGestureRecognizerStateEnded) {
         
-        if (x >= Width / 2.f) {
-            
-            // 初始化3D变换,获取默认值
-            CATransform3D perspectiveTransform = CATransform3DIdentity;
-            
-            // 透视
-            perspectiveTransform.m34 = -1.0/2000.0;
-            
-            // 空间旋转
-            perspectiveTransform = CATransform3DRotate(perspectiveTransform, [Math radianFromDegree:180], 0, 1, 0);
-            
-            [CATransaction setDisableActions:NO];
-            _layer.transform = perspectiveTransform;
-            
-        } else {
-            
-            // 初始化3D变换,获取默认值
-            CATransform3D perspectiveTransform = CATransform3DIdentity;
-            
-            // 透视
-            perspectiveTransform.m34 = -1.0/2000.0;
-            
-            // 空间旋转
-            perspectiveTransform = CATransform3DRotate(perspectiveTransform, [Math radianFromDegree:0], 0, 1, 0);
-            
-            [CATransaction setDisableActions:NO];
-            _layer.transform = perspectiveTransform;
-        }
+        // 初始化3D变换,获取默认值
+        CATransform3D perspectiveTransform = CATransform3DIdentity;
+        
+        // 透视
+        perspectiveTransform.m34 = -1.0/2000.0;
+        
+        // 空间旋转
+        perspectiveTransform = CATransform3DRotate(perspectiveTransform, [Math radianFromDegree:x >= Width / 2.0f ? 180 : 0], 0, 1, 0);
+        
+        [CATransaction setDisableActions:NO];
+        _layer.transform = perspectiveTransform;
     }
 }
 
