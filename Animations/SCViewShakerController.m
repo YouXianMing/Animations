fed43846680d72dba787c910d91a5eeb21164f8b
YouXianMing <xian.you@ajmall-group.com>
2020-06-18 10:51:04 +0800

    Update SCViewShakerController.m
    Fix the problem that the CPU reaches 100% after exiting the SCViewShakerController.

--- Animations/SCViewShakerController.m
+++ Animations/SCViewShakerController.m
@@ -10,6 +10,12 @@
 #import "UIView+SetRect.h"
 #import "UIView+Shake.h"
 
+@interface SCViewShakerController ()
+
+@property (nonatomic, strong) UILabel *label;
+
+@end
+
 @implementation SCViewShakerController
 
 - (void)viewDidLoad {
@@ -18,35 +24,40 @@
     
     // https://github.com/rFlex/SCViewShaker
     
-    UILabel *label      = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    label.textAlignment = NSTextAlignmentCenter;
-    label.center        = self.contentView.middlePoint;
-    label.text          = @"😂";
-    label.font          = [UIFont systemFontOfSize:100.f];
-    [self.contentView addSubview:label];
+    self.label               = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.center        = self.contentView.middlePoint;
+    self.label.text          = @"😂";
+    self.label.font          = [UIFont systemFontOfSize:100.f];
+    [self.contentView addSubview:self.label];
     
     static int i = 0;
     switch (i++ % 4) {
             
         case 0:
-            [label shakeWithOptions:SCShakeOptionsDirectionRotate | SCShakeOptionsForceInterpolationExpDown | SCShakeOptionsAtEndRestart | SCShakeOptionsAutoreverse force:0.15 duration:1 iterationDuration:0.03 completionHandler:nil];
+            [self.label shakeWithOptions:SCShakeOptionsDirectionRotate | SCShakeOptionsForceInterpolationExpDown | SCShakeOptionsAtEndRestart | SCShakeOptionsAutoreverse force:0.15 duration:1 iterationDuration:0.03 completionHandler:nil];
             break;
             
         case 1:
-            [label shakeWithOptions:SCShakeOptionsDirectionHorizontal | SCShakeOptionsForceInterpolationNone | SCShakeOptionsAtEndContinue force:0.05 duration:1 iterationDuration:0.03 completionHandler:nil];
+            [self.label shakeWithOptions:SCShakeOptionsDirectionHorizontal | SCShakeOptionsForceInterpolationNone | SCShakeOptionsAtEndContinue force:0.05 duration:1 iterationDuration:0.03 completionHandler:nil];
             break;
             
         case 2:
-            [label shakeWithOptions:SCShakeOptionsDirectionHorizontalAndVertical | SCShakeOptionsForceInterpolationRandom | SCShakeOptionsAtEndContinue force:0.3 duration:1 iterationDuration:0.02 completionHandler:nil];
+            [self.label shakeWithOptions:SCShakeOptionsDirectionHorizontalAndVertical | SCShakeOptionsForceInterpolationRandom | SCShakeOptionsAtEndContinue force:0.3 duration:1 iterationDuration:0.02 completionHandler:nil];
             break;
             
         case 3:
-            [label shakeWithOptions:SCShakeOptionsDirectionRotate | SCShakeOptionsForceInterpolationExpDown | SCShakeOptionsAtEndRestart force:0.15 duration:0.75 iterationDuration:0.03 completionHandler:nil];
+            [self.label shakeWithOptions:SCShakeOptionsDirectionRotate | SCShakeOptionsForceInterpolationExpDown | SCShakeOptionsAtEndRestart force:0.15 duration:0.75 iterationDuration:0.03 completionHandler:nil];
             break;
             
         default:
             break;
     }
+}
+
+- (void)dealloc {
+
+    [self.label endShake];
 }
 
 @end
